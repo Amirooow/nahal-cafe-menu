46a97368cc467bd9120e566efb1f6612bc86458c
Amirooow <145593406+Amirooow@users.noreply.github.com>
2025-08-12 16:28:28 +0330

    n1

---
+++ script.js
@@ -0,0 +1,119 @@
+let menuCategory = document.querySelector('#menuCategory');
+
+let cardContainer = document.querySelector('#cardsContainer');
+let test = document.querySelector('.fruitJuice');
+
+let specialIcecream = [ 
+    {name : 'معجون' , price : '200000' , img : './image/maajoon.png'} ,
+    {name : 'کیک بستنی ویژه' , price : '160000' , img : './image/cakebastani vije.png'} , 
+    { name : 'کیک بستنی' , price :'115000' , img : './image/cake bastani.png'}, { name : 'شیر موز' , price :'70000' , img : './image/shirmoz.png'} , { name : 'شیرموز پسته' , price :'100000' , img : './image/shirpeste.png'} , { name : 'شیر موز ویژه' , price :'160000' , img : './image/shirmoz vije.png'} , { name : 'شیک' , price :'90000' , img : './image/shake.png'} , { name : 'آیس پک' , price :'80000' , img : './image/ice pack.png'} , { name : 'فالوده بستنی' , price :'60000' , img : './image/faludebastani.png'} , { name : 'آب هویج بستنی' , price :'50000' , img : './image/havijbastani.png'}
+]
+
+let cake = [ 
+    {name : 'ساچر' , price : '60000' , img : './image/sacher.png'} ,
+    {name : 'هویج گردو' , price : '50000' , img : './image/havij gerdoo.png'} , 
+    { name : 'کوکی موکا ', price :'115000' , img : './image/coocky.png'} ,
+    { name : 'کوکی رژیمی ', price :'115000' , img : './image/cookie rejimi.png'} ,
+    { name : 'موچی ', price :'115000' , img : './image/moochi.png'} ,
+    { name : 'جار کیک ', price :'115000' , img : './image/deser.png'} ,
+    { name : 'شیرینی تر ', price :'115000' , img : './image/shirini tar.png'} ,
+    { name : 'نون خامه ای ', price :'115000' , img : './image/noon khame ei.png'} ,
+]
+
+let softIceCream = [ 
+    {name : 'بستنی دستگاهی' , price : '80000' , img : './image/ghifi.png'} ,
+    {name : 'بستنی دو رنگ ' , price : '80000' , img : './image/ghifi 2 rang.png'} , 
+    { name : ' قیفی پسته ای ', price :'60000' , img : './image/ghifi peste e.png'} ,
+    { name : ' بستنی ژلاتو ', price :'20000' , img : './image/gelato.png'} ,
+]
+
+
+let hotBar = [ 
+    {name : 'اسپرسو' , price : '60000' , img : './image/essepreso.png'} ,
+    {name : ' لاته' , price : '50000' , img : './image/cappo.png'} , 
+    { name : 'آفوگاتو', price :'115000' , img : './image/afogato.png'} ,
+    { name : 'نوشیدنی گرم ', price :'115000' , img : './image/hot chocolate.png'} ,
+]
+
+let juice = [ 
+    {name : 'آب هویج' , price : '60000' , img : './image/layer 18.png'}
+]
+// اینجا یه ایونت گذاشتم که وقتی یوزر کلیک میکنه بفهمم رو چه کتگوری کلیک کرده بعد یه شرط گذاشتم که تو کلاس لیستش بگره اگه اون کتگوری بود و ارایه مورد نظر رو به فانکشن ایینی پاس بده 
+
+menuCategory.addEventListener('click' , (e)=>{
+    const t = e.target;
+        console.log(t.className);
+        console.log(t);
+        let test = t.classList;
+        // console.log(test);
+        
+        
+    if(t.className == 'proIcecream'){
+        cardContainer.innerHTML = '';
+        loadingItems(specialIcecream);
+        menuCategory.children.forEach( (e)=>{
+            e.style.color = '#959595'
+        })
+        t.style.color = 'white';
+        
+    }else if(t.className == 'cake'){
+        cardContainer.innerHTML = '';
+        loadingItems(cake);
+        menuCategory.children.forEach( (e)=>{
+        e.style.color = '#959595'
+        })
+        t.style.color = 'white';
+
+    }else if(t.className == 'iceCream'){
+        cardContainer.innerHTML = '';
+        loadingItems(softIceCream);
+        menuCategory.children.forEach( (e)=>{
+        e.style.color = '#959595'
+        })
+        t.style.color = 'white';
+
+    }else if(t.className == 'fruitJuice'){
+        cardContainer.innerHTML = '';
+        loadingItems(juice);
+        menuCategory.children.forEach( (e)=>{
+        e.style.color = '#959595'
+        })
+        t.style.color = 'white';
+
+    }else if(t.className == 'hotBar'){
+        cardContainer.innerHTML = '';
+        loadingItems(hotBar);
+        menuCategory.children.forEach( (e)=>{
+        e.style.color = '#959595'
+        })
+        t.style.color = 'white';
+
+    }
+})
+
+
+
+function loadingItems(category){
+console.log('lllllllllllll');
+
+    category.forEach(e => {
+    let cardDiv = document.createElement("div");
+    let imageBox = document.createElement("div");
+    let image = document.createElement('img');
+    let itemName = document.createElement('div');
+    let itemPrice = document.createElement('div');
+    image.src = e.img;
+    cardDiv.className = 'card';
+    imageBox.className = 'imageBox';
+    itemName.className = 'itemName';
+    itemPrice.className = 'Price';
+    itemName.innerText = e.name;
+    itemPrice.innerText = e.price
+    imageBox.appendChild(image);
+    cardDiv.appendChild(imageBox);
+    cardDiv.appendChild(itemName);
+    cardDiv.appendChild(itemPrice);
+    cardContainer.appendChild(cardDiv);
+    
+    });
+}
